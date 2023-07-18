d5bc7ff423233a14ac4f44f2cb76ae51da482d09
Shreyash <sdwadmalwar@gmail.com>
2023-07-18 21:29:50 +0530

    Adding Block course team member plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c88423a4810bc4944311d8f6a3ff0b5f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b2cccb69e6d6ccfe46e8');
